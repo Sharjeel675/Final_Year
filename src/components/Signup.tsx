fa76f4058dfd0cc534bbc413dc7c7b9ca95974aa
Sharjeel675 <94467411+Sharjeel675@users.noreply.github.com>
2023-12-19 19:48:41 +0500

    some changes

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -11,9 +11,30 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Link from "next/link";
 
 const Signup = () => {
-  return <button className="p-4 text-xs">Login/Signup</button>;
+  const { user } = useUser();
+  return (
+    <button className="p-4 text-xs">
+      {" "}
+      {!user && (
+        <>
+          <Link href={"/api/auth/signup"}>
+            <h1>Login</h1>{" "}
+          </Link>
+        </>
+      )}
+      {user && (
+        <>
+          <Link href={"/api/auth/logout"}>
+            <h1>logout</h1>
+          </Link>
+        </>
+      )}
+    </button>
+  );
 };
 
 export default Signup;
